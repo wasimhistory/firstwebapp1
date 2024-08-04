1f3a39c55ffbd25b6eeeb5664143922263abd41d
wasimhistory <wasimhistory@gmail.com>
2024-08-04 19:22:31 +0530

    initial commit - webapp1

---
+++ functions.py
@@ -0,0 +1,15 @@
+FILEPATH = r"C:\Users\Wasim\PycharmProjects\pythonProject\.venv\webapp1\todos.txt"
+
+def get_todos(filepath=FILEPATH):
+    with open(filepath,'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todos_arg,filepath=FILEPATH):
+    with open(filepath,'w') as file:
+        file.writelines(todos_arg)
+
+if __name__ == "__main__":
+     print("You are in functions.py, below output from todos.txt")
+     print(get_todos(FILEPATH))
+
